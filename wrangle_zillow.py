edc8dbb0b31d76402dfdd0d62b89f3321250ec1f
Zach Gulde <zachgulde@gmail.com>
2022-04-01 15:00:23 -0500

    Move zillow wrangle code into module

---
+++ wrangle_zillow.py
@@ -0,0 +1,79 @@
+import os
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+import env
+
+query = '''
+SELECT
+    prop.*,
+    predictions_2017.logerror,
+    predictions_2017.transactiondate,
+    air.airconditioningdesc,
+    arch.architecturalstyledesc,
+    build.buildingclassdesc,
+    heat.heatingorsystemdesc,
+    landuse.propertylandusedesc,
+    story.storydesc,
+    construct.typeconstructiondesc
+FROM properties_2017 prop
+JOIN (
+    SELECT parcelid, MAX(transactiondate) AS max_transactiondate
+    FROM predictions_2017
+    GROUP BY parcelid
+) pred USING(parcelid)
+JOIN predictions_2017 ON pred.parcelid = predictions_2017.parcelid
+                      AND pred.max_transactiondate = predictions_2017.transactiondate
+LEFT JOIN airconditioningtype air USING (airconditioningtypeid)
+LEFT JOIN architecturalstyletype arch USING (architecturalstyletypeid)
+LEFT JOIN buildingclasstype build USING (buildingclasstypeid)
+LEFT JOIN heatingorsystemtype heat USING (heatingorsystemtypeid)
+LEFT JOIN propertylandusetype landuse USING (propertylandusetypeid)
+LEFT JOIN storytype story USING (storytypeid)
+LEFT JOIN typeconstructiontype construct USING (typeconstructiontypeid)
+WHERE prop.latitude IS NOT NULL
+  AND prop.longitude IS NOT NULL
+  AND transactiondate <= '2017-12-31'
+'''
+
+def overview(df):
+    print('--- Shape: {}'.format(df.shape))
+    print('--- Info')
+    df.info()
+    print('--- Column Descriptions')
+    print(df.describe(include='all'))
+
+def nulls_by_columns(df):
+    return pd.concat([
+        df.isna().sum().rename('count'),
+        df.isna().mean().rename('percent')
+    ], axis=1)
+
+def nulls_by_rows(df):
+    return pd.concat([
+        df.isna().sum(axis=1).rename('n_missing'),
+        df.isna().mean(axis=1).rename('percent_missing'),
+    ], axis=1).value_counts().sort_index()
+
+def handle_missing_values(df, prop_required_column, prop_required_row):
+    n_required_column = round(df.shape[0] * prop_required_column)
+    n_required_row = round(df.shape[1] * prop_required_row)
+    df = df.dropna(axis=0, thresh=n_required_row)
+    df = df.dropna(axis=1, thresh=n_required_column)
+    return df
+
+def acquire():
+    if os.path.exists('zillow.csv'):
+        df = pd.read_csv('zillow.csv')
+    else:
+        database = 'zillow'
+        url = f'mysql+pymysql://{env.user}:{env.password}@{env.host}/{database}'
+        df = pd.read_sql(query, url)
+        df.to_csv('zillow.csv', index=False)
+    return df
